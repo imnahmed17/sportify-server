47edffb20c35f0d1641db145c107ea989e27c8eb
helloashik17 <asiknoman150@gmail.com>
2023-06-12 02:32:26 +0600

    class status update done

--- index.js
+++ index.js
@@ -169,6 +169,21 @@ async function run() {
             res.send(result);
         });
 
+        app.patch('/classes/feedback/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const document = req.body;
+            const updateDoc = {
+                $set: {
+                    status: document.status,
+                    feedback: document.feedback
+                }
+            };
+
+            const result = await classCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
